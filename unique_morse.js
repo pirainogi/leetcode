5c688e03f25abb5aedf96f396abd0b7d3d1022d0
pirainogi <pirainogi@gmail.com>
2019-11-09 16:54:32 -0500

    grammar/formatting

--- unique_morse.js
+++ unique_morse.js
@@ -1,4 +1,4 @@
-// nternational Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+//International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
 
 // Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-..--...", (which is the concatenation "-.-." + ".-" + "-..."). We'll call such a concatenation, the transformation of a word.
 
@@ -7,8 +7,7 @@
 // Example:
 // Input: words = ["gin", "zen", "gig", "msg"]
 // Output: 2
-// Explanation:
-// The transformation of each word is:
+// Explanation: The transformation of each word is:
 // "gin" -> "--...-."
 // "zen" -> "--...-."
 // "gig" -> "--...--."
