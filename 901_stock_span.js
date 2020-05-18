544674973cbf0a86df2b3b3ebe196f3adb39cf97
pirainogi <pirainogi@gmail.com>
2020-05-18 16:34:53 -0400

    stock span

---
+++ 901_stock_span.js
@@ -0,0 +1,12 @@
+function calculateSpan(arr){
+  let output = [1]
+  for(let curr = 1; curr < arr.length; curr++){
+    output[curr] = 1
+    let prev = curr - 1 
+    while (prev >= 0 && arr[curr] >= arr[prev]){
+      output[curr] += 1
+      prev-- 
+    }
+  }
+  return output 
+}
