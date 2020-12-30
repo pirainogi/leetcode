8b4f6a52022d0329f2ddc2b264004662edc49096
pirainogi <pirainogi@gmail.com>
2020-12-30 10:05:24 -0500

    301-398 files

--- 3_longest_substring.js
+++ 3_longest_substring.js
@@ -1,10 +1,10 @@
-/*Given a string, find the length of the longest substring without 
+/*Given a string, find the length of the longest substring without
 repeating characters.
 
 Example 1:
 Input: "abcabcbb"
-Output: 3 
-Explanation: The answer is "abc", with the length of 3. 
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
 
 Example 2:
 Input: "bbbbb"
@@ -14,14 +14,29 @@ Explanation: The answer is "b", with the length of 1.
 Example 3:
 Input: "pwwkew"
 Output: 3
-Explanation: The answer is "wke", with the length of 3. 
+Explanation: The answer is "wke", with the length of 3.
 Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
+
+const longestSubstring = (str) => {
+  let map = {}, start = 0, maxLength = 0; arr = str.split("")
+
+  for(let i = 0; i < arr.length; i++) {
+    let curr = map[arr[i]]
+    if(curr !== null && start <= curr) {
+      start = curr + 1
+    } else {
+      maxLength = Math.max(maxLength, i - start + 1)
+    }
+    map[arr[i]] = i
+  }
+  return maxLength 
+}
 
 const lengthOfLongestSubstring = function(s) {
   let set = new Set();
   let left = 0, right = 0, maxLength = 0;
-  
+
   while(right < s.length){
       if(!set.has(s.charAt(right))){
           set.add(s.charAt(right))
@@ -29,7 +44,7 @@ const lengthOfLongestSubstring = function(s) {
           right ++
       } else {
           set.delete(s.charAt(left))
-          left ++ 
+          left ++
       }
   }
   return maxLength
--- 901_stock_span.js
+++ 901_stock_span.js
@@ -2,11 +2,11 @@ function calculateSpan(arr){
   let output = [1]
   for(let curr = 1; curr < arr.length; curr++){
     output[curr] = 1
-    let prev = curr - 1 
+    let prev = curr - 1
     while (prev >= 0 && arr[curr] >= arr[prev]){
       output[curr] += 1
-      prev-- 
+      prev--
     }
   }
-  return output 
+  return output
 }
